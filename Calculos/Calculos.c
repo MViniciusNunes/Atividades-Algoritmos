b542c7f4ef868e75510f35037ea5aff5392fafaa
Vinicius <vinynunes32@gmail.com>
2024-09-19 19:01:36 -0300

    algumas mudança

---
+++ Calculos/Calculos.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1,num2,soma,subt,prod,div;
+    
+    printf("Entre com um numero: ");
+    scanf("%f", &num1);
+
+    printf("Entre com outro numero: ");
+    scanf("%f", &num2);
+
+    soma=num1+num2;
+    subt=num1-num2;
+    prod=num1*num2;
+    div=num1/num2;
+
+    printf("\tA soma eh: \t\t%2.2f\n", soma);
+     printf("\tA subtracao eh: \t%.2f\n", subt);
+      printf("\tA multiplicacao eh: \t%.2f\n", prod);
+       printf("\tA divisao eh: \t\t%2.2f\n", div);
+    return 0;
+}
